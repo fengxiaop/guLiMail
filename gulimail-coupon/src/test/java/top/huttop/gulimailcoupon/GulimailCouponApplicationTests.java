a18434370bcaf38d8b0260917d8eedf99543b003
“wind” <1493020035@qq.com>
2022-07-04 15:16:01 +0800

    项目初始化结构

---
+++ gulimail-coupon/src/test/java/top/huttop/gulimailcoupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.huttop.gulimailcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimail-order/src/test/java/top/huttop/gulimailorder/GulimailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package top.huttop.gulimailorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
